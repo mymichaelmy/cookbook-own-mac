e61b4997880f581e31a66a95b4de9eb397afe4d0
michaelyang <mymichaelmy@gmail.com>
2016-08-01 18:32:26 -0700

    working on search

--- js/routes.js
+++ js/routes.js
@@ -1,17 +1,24 @@
+var rootURL="127.0.0.1";   //save root url
+
 angular.module('cookbook')
 .config(['$routeProvider',function($routeProvider){
     $routeProvider.
         when('/notes', {
-        templateUrl: 'templates/index-template.html',
-        controller: 'cardsController'
+            templateUrl: 'templates/index-template.html',
+            controller: 'cardsController'
         })
         .when('/', {
-        templateUrl: 'templates/index-template.html',
-        controller: 'cardsController'
+            templateUrl: 'templates/index-template.html',
+            controller: 'cardsController'
         })
         .when('/methods/:cardUid', {
-        templateUrl: 'templates/methods-template.html',
-        controller: 'cardDetailController'
+            templateUrl: 'templates/methods-template.html',
+            controller: 'cardDetailController'
+        })
+        .when('/search/:searchTerm', {
+            templateUrl: 'search/search-template.html',
+            controller: 'searchResultController'
+
         })
         .otherwise({
             redirectTo: "/"
@@ -38,6 +45,17 @@ angular.module('cookbook').controller('cardDetailController',  function($scope, 
   // $scope.order_id = $routeParams.orderId;
     $http.get('/drupal/rest/node/'+$routeParams.cardUid).success(function(data)
     {
-        $scope.card=data;   //good
+        $scope.card=data;
+    });
+});
+
+
+//search resutl controller
+angular.module('cookbook').controller('searchResultController',  function($scope, $http,$routeParams) {
+
+    var url="/:8983/solr/drupal/select?q="+$routeParams.searchTerm+"&wt=json&indent=true&hl=true&fq=ss_language:und";
+    $http.get(url).success(function(data)
+    {
+        $scope.results=data;
     });
 });
