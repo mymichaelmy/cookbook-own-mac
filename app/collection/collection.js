dd5b08a093a777fabee5491cc81064d83f387f12
michaelyang <mymichaelmy@gmail.com>
2016-09-02 17:11:30 -0700

    working on collection

--- app/collection/collection.js
+++ app/collection/collection.js
@@ -7,15 +7,43 @@ angular.module('cookbook').controller('collectionController',function($scope,$ht
 		
 		$http.get('/drupal/rest/node/'+value.cardID).success(function(data)
 		{
+			//set up the card obj and push it into the array used in ng-repeat
 			var obj=
 			{
 				title:data.title,
 				imgSrc:"/drupal/sites/default/files/"+data.field_card_image.und[0].filename,
 				summary:data.field_activity_description.und[0].value,
+				category:data.type,
 				nid:data.nid,
 				zipName:data.field_zip_files.und[0].filename,
-				fileURL:rootURL+mainPort+"/drupal/sites/default/files/"+data.field_zip_files.und[0].filename
+				fileURL:rootURL+mainPort+"/drupal/sites/default/files/"+data.field_zip_files.und[0].filename,
+				
 			};
+
+			if(data.field_author)
+			{
+				obj.author=data.field_author.und[0].value;
+			}
+			else
+			{
+				obj.author='anonymous';
+			}
+			if(data.field_recommended.und)
+			{
+				obj.recommended=data.field_recommended.und[0].value;
+			}
+			else
+			{
+				obj.recommended=0;
+			}
+			if(data.field_endorse.und)
+			{
+				obj.endorse=data.field_endorse.und[0].value;
+			}
+			else
+			{
+				obj.endorse='';
+			}
 
 			outputArray.push(obj);
 
