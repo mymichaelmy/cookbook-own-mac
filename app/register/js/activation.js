91b99d5133d625f0bdb3435240ae8836547d1442
michaelyang <mymichaelmy@gmail.com>
2016-10-28 13:53:07 -0700

    fixed a cxsf

--- app/register/js/activation.js
+++ app/register/js/activation.js
@@ -10,30 +10,30 @@ angular.module('cookbook').controller('activationController',function($http,$sco
 			
 		};
 
-		var config = {
-			headers:{
-				'Content-Type': 'application/json'
-			},
-		};
+		var configPost = {
+				headers:{
+					'Content-Type': 'application/json',
+				},
+			};
+		var dataPost=
+		{
 
-		$http.post('drupal/userapi/user/user_pass_reset.json',submitObj,config).success(function(data)   //need to modify ip address before upload
+		};
+		$http.post('/drupal/userapi/user/token.json',dataPost,configPost).success(function(data)  //gain x-csrf token
 		{
 			console.log(data);
-			$scope.token=data.pass_reset_token;
-			// var passData=
-			// {
-			// 	"pass": $scope.password,
-			// 	"pass1": $scope.password,
-			// 	"pass2": $scope.password
-   //          };
 
-			// $http.put('drupal/userapi/user/'+$scope.uid+'.json?pass-reset-token='+data.pass_reset_token,passData).success(function(data)
-			// {
-			// 	console.log(data);
-			// }).error(function(data, status)
-			// {
-			// 	console.error('error', status, data);
-			// });
+			var config = {
+				headers:{
+					'Content-Type': 'application/json',
+					'X-CSRF-Token': data.token
+				},
+			};
+			$http.post('drupal/userapi/user/user_pass_reset.json',submitObj,config).success(function(data)   //need to modify ip address before upload
+			{
+				console.log(data);
+				$scope.token=data.pass_reset_token;
+			});
 		});
 
 
