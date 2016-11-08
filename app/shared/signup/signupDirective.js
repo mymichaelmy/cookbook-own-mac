2d2286c47bce4be4abd12d63774bed8e4bc3af3a
michaelyang <mymichaelmy@gmail.com>
2016-11-07 17:04:11 -0800

    adding signup directive

---
+++ app/shared/signup/signupDirective.js
@@ -0,0 +1,13 @@
+angular.module('cookbook').directive('signUp', function() {
+	return {
+
+		restrict: 'E',
+		// replace:'true',
+		templateUrl:'/app/shared/signup/signupDirective.html',
+		link:function(scope,elem,attr)
+		{
+
+		}
+	};
+
+});
