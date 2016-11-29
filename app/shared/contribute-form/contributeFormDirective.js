786b713b4ef45de59b6e1a4ca8207473f20fc75b
michaelyang <mymichaelmy@gmail.com>
2016-11-29 12:35:17 -0800

    fixing reupload function

--- app/shared/contribute-form/contributeFormDirective.js
+++ app/shared/contribute-form/contributeFormDirective.js
@@ -12,7 +12,8 @@ angular.module('cookbook').directive('contributeForm',function($http,commonServi
 			scope.barOpacity=0.8;   //opacity of the bar
 			scope.backColor='#25B135';
 			scope.borderStyle='none';
-
+			scope.progress=0;
+			scope.progressShow=false; //decide the show of the bar
 			scope.form.filenamePlaceholder='';
 
 			scope.contributeButton=function(status)
@@ -32,11 +33,22 @@ angular.module('cookbook').directive('contributeForm',function($http,commonServi
 			scope.quitContributeButton=function()
 			{
 				scope.contributeForm.hide=true;
+				element[0].reset();
+				scope.progressShow=false;
 			};
 
 			scope.onAfterValidateFunc=function()
 			{
+				scope.progressShow=true;
+				scope.barOpacity=0.8;
 				commonService.startUploadFile(scope.form.file.filename,scope.form.file.base64,'alsoSee',scope);
+			};
+
+			scope.startLoad=function()
+			{
+				scope.progressShow=true;
+				scope.barOpacity=0.8;
+				scope.progress=2;
 			};
 		}
 	};
